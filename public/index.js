81e54f5d009783e3499bf77e69af2284b7b2a683
@theophilous7 <theophilous789@gmail.com>
2024-01-14 02:40:20 +0100

    src update

--- public/index.js
+++ public/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from '../src/App';
+import reportWebVitals from '../src/reportWebVitals';
 import {HashRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchPage from './containers/pages/searchpage/searchPage';
-import PoliticsPage from './containers/pages/politics/politics';
-import BusinessPage from './containers/pages/business/business';
-import TechnologyPage from './containers/pages/technology/technology';
-import TravelsPage from './containers/pages/travels/travels';
-import WeatherPage from './containers/pages/weather/weather';
-import ReligionPage from './containers/pages/religion/religion';
-import HealthPage from './containers/pages/health&fitness/health&fitness';
-import MusicPage from './containers/pages/music/music';
-import SportsPage from './containers/pages/sports/sports';
+import SearchPage from '../src/containers/pages/searchpage/searchPage';
+import PoliticsPage from '../src/containers/pages/politics/politics';
+import BusinessPage from '../src/containers/pages/business/business';
+import TechnologyPage from '../src/containers/pages/technology/technology';
+import TravelsPage from '../src/containers/pages/travels/travels';
+import WeatherPage from '../src/containers/pages/weather/weather';
+import ReligionPage from '../src/containers/pages/religion/religion';
+import HealthPage from '../src/containers/pages/health&fitness/health&fitness';
+import MusicPage from '../src/containers/pages/music/music';
+import SportsPage from '../src/containers/pages/sports/sports';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
